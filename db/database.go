2da611f67b4033c552351cd06bc2af7718e19024
dhirajpatra <dhiraj.patra@gmail.com>
2024-03-16 21:25:13 +0530

    updated

--- db/database.go
+++ db/database.go
@@ -81,10 +81,30 @@ func main() {
         name varchar(255),
         age int,
         salary int)`
+	err = CreateTable(db, query)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Table created!")
+
+	query = `insert into employees (id, name, age, salary) values (1, 'John', 30, 5000)`
+	_, err = db.Exec(query)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Data inserted!")
+
+	query = `CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );`
 
 	err = CreateTable(db, query)
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Println("Table created!")
+	db.Close()
 }
